e823cfb2eeadac790bee9cd5e8d75c352d5a4123
denghui.li <3379489032@qq.com>
2024-09-26 21:53:54 +0800

    update type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-type fn = Function;
+type Fn<T> = (...args: any[]) => Promise<T>
 
-export default function resolver<T, E>(fn: fn) {
-  return async (...args: any[]) => {
-    try {
-      const data: T = await fn(...args);
-      return [null, data] as const;
-    } catch (e) {
-      return [e as E, null] as const;
+export default function resolver<T, E>(fn: Fn<T>) {
+    return async (...args: any[]) => {
+        try {
+            const data: T = await fn(...args)
+            return [null, data] as const
+        } catch (e) {
+            return [e as E, null] as const
+        }
     }
-  };
 }
